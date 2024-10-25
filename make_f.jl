84edf41f059e7b17681808e8ef63df52dffd5376
Selene <pomc.ecc@gmail.com>
2024-10-25 17:29:30 -0300

    Implementing qexp and expa functions.

--- make_f.jl
+++ make_f.jl
@@ -21,7 +21,7 @@ function make_f(
     )
         xcar = copy(car0)
         xcar[isnan.(xcar)] .= xar
-        xc = make_wfstructs(xcar, orbs)
+        xc = make_wfstr_nGum(xcar, orbs)
         gnt = [Array{Array}(undef, 0) for _ = lnα]
         for ixc = xc
             push!(gnt[ixc.l], weight_funtion(ixc, lnα[ixc.l]))
@@ -37,7 +37,97 @@ function make_f(
     )
         xcar = copy(car0)
         xcar[isnan.(xcar)] .= xar
-        return make_wfstructs(xcar, orbs)
+        return make_wfstr_nGum(xcar, orbs)
+    end
+
+    return f_opt, xar0, (car0, orbs, lnα, al, Ht, Vt, ne), get_x
+end
+
+function make_f(
+    qt::Array{ETabn},
+    x0::Array{nGuma},
+    op::Array{nGuma_opt},
+    ζ::Int16,
+    ne::Int16
+)
+    lnα = [calc_lnα(iqt) for iqt = qt]
+    al = [exp.(ilnα) for ilnα = lnα]
+    Ht, Vt = density_prep(al..., ζ)
+    xar0, car0, orbs = make_array(x0, op)
+    function f_opt(
+        xar::Array{Float64, 1},
+        car0::Array{Float64, 1},
+        orbs::Array{Tuple, 1},
+        lnα::Array,
+        al::Array,
+        Ht::Tuple,
+        Vt::Tuple,
+        ne::Int16
+    )
+        xcar = copy(car0)
+        xcar[isnan.(xcar)] .= xar
+        xc = make_wfstr_nGuma(xcar, orbs)
+        gnt = [Array{Array}(undef, 0) for _ = lnα]
+        for ixc = xc
+            push!(gnt[ixc.l], weight_funtion(ixc, lnα[ixc.l]))
+        end
+        gn = [hcat(ignt...) for ignt = gnt]
+        return energy(al..., gn..., Ht, Vt, ne)
+    end
+
+    function get_x(
+        xar::Array{Float64, 1},
+        car0::Array{Float64, 1},
+        orbs::Array{Tuple, 1},
+    )
+        xcar = copy(car0)
+        xcar[isnan.(xcar)] .= xar
+        return make_wfstr_nGuma(xcar, orbs)
+    end
+
+    return f_opt, xar0, (car0, orbs, lnα, al, Ht, Vt, ne), get_x
+end
+
+function make_f(
+    qt::Array{ETabn},
+    x0::Array{nGumqe},
+    op::Array{nGumqe_opt},
+    ζ::Int16,
+    ne::Int16
+)
+    lnα = [calc_lnα(iqt) for iqt = qt]
+    al = [exp.(ilnα) for ilnα = lnα]
+    Ht, Vt = density_prep(al..., ζ)
+    xar0, car0, orbs = make_array(x0, op)
+    function f_opt(
+        xar::Array{Float64, 1},
+        car0::Array{Float64, 1},
+        orbs::Array{Tuple, 1},
+        lnα::Array,
+        al::Array,
+        Ht::Tuple,
+        Vt::Tuple,
+        ne::Int16
+    )
+        xcar = copy(car0)
+        xcar[isnan.(xcar)] .= xar
+        xc = make_wfstr_nGumqe(xcar, orbs)
+        gnt = [Array{Array}(undef, 0) for _ = lnα]
+        for ixc = xc
+            push!(gnt[ixc.l], weight_funtion(ixc, lnα[ixc.l]))
+        end
+        gn = [hcat(ignt...) for ignt = gnt]
+        return energy(al..., gn..., Ht, Vt, ne)
+    end
+
+    function get_x(
+        xar::Array{Float64, 1},
+        car0::Array{Float64, 1},
+        orbs::Array{Tuple, 1},
+    )
+        xcar = copy(car0)
+        xcar[isnan.(xcar)] .= xar
+        return make_wfstr_nGumqe(xcar, orbs)
     end
 
     return f_opt, xar0, (car0, orbs, lnα, al, Ht, Vt, ne), get_x
--- weight_function.jl
+++ weight_function.jl
@@ -14,8 +14,84 @@ struct nGum_opt
     d::Array{Bool, 1}
 end
 
+struct nGuma
+    n::Int8
+    l::Int8
+    r::Array{Float64, 1}
+    a::Array{Float64, 1}
+    c::Array{Float64, 1}
+    d::Array{Float64, 1}
+end
+
+struct nGuma_opt
+    r::Array{Bool, 1}
+    a::Array{Bool, 1}
+    c::Array{Bool, 1}
+    d::Array{Bool, 1}
+end
+
+struct nGumqe
+    a::Float64
+    n::Int8
+    l::Int8
+    r::Array{Float64, 1}
+    c::Array{Float64, 1}
+    q::Array{Float64, 1}
+    d::Array{Float64, 1}
+end
+
+struct nGumqe_opt
+    a::Bool
+    r::Array{Bool, 1}
+    c::Array{Bool, 1}
+    q::Array{Bool, 1}
+    d::Array{Bool, 1}
+end
+
+function qexp(x::Real, q)
+    if q == 1
+        return exp.(x)
+    elseif q < 1
+        if x < -1/(1-q)
+            return 0
+        else
+            return (1 + (1-q)*x)^(1/(1-q))
+        end
+    else
+        if x > -1/(1-q)
+            return Inf
+        else
+            return (1 + (1-q)*x)^(1/(1-q))
+        end
+    end
+end
+
+function qexp(x::Array, q)
+    return [qexp(ix, q) for ix = x]
+end
+
+
 function weight_funtion(x::nGum, lnα)
-    gum = exp.(-exp.(-x.a*(lnα .- x.d'))) * x.c
+    gum = exp.(-exp.(-abs(x.a)*(lnα .- x.d'))) * abs.(x.c)
+    gum .*= exp.(-1/2*lnα)
+    for i = 1:x.n-x.l
+        gum .*= (exp.(-(lnα .- x.r[i])) .- 1)
+    end
+    return gum
+end
+
+function weight_funtion(x::nGuma, lnα)
+    gum = exp.(-exp.(-x.a'.*(lnα .- x.d'))) * x.c
+    gum .*= exp.(-1/2*lnα)
+    for i = 1:x.n-x.l
+        gum .*= (exp.(-(lnα .- x.r[i])) .- 1)
+    end
+    return gum
+end
+
+function weight_funtion(x::nGumqe, lnα)
+    gum = [qexp(-qexp(-abs(x.a)*(ilnα - x.d[j]),x.q[j]),x.q[j]) for ilnα = lnα, j = eachindex(x.c)]
+    gum = gum * abs.(x.c)
     gum .*= exp.(-1/2*lnα)
     for i = 1:x.n-x.l
         gum .*= (exp.(-(lnα .- x.r[i])) .- 1)
@@ -58,7 +134,77 @@ function make_array(x::Array{nGum}, op::Array{nGum_opt})
     return xar, car, orbs
 end
 
-function make_wfstructs(xcar::Array{Float64, 1}, orbs::Array{Tuple, 1})
+function make_array(x::Array{nGuma}, op::Array{nGuma_opt})
+    xar = Array{Float64}(undef, 0)
+    car = Array{Float64}(undef, 0)
+    orbs = Array{Tuple}(undef, 0)
+    for i = 1:size(x,1)
+        nci = size(car,1)
+        n = x[i].n
+        l = x[i].l
+        for name = fieldnames(typeof(x[i]))
+            name in [:n, :l] ? continue :
+            prop = getproperty(x[i], name)
+            opprop = getproperty(op[i], name)
+            if opprop isa Bool
+                if opprop
+                    append!(xar, prop)
+                    append!(car, fill(NaN, size(prop,1)))
+                else
+                    append!(car, prop)
+                end
+            else
+                for j = eachindex(opprop)
+                    if opprop[j]
+                        append!(xar, prop[j])
+                        append!(car, NaN)
+                    else
+                        append!(car, prop[j])
+                    end
+                end
+            end
+        end
+        push!(orbs, (size(car,1)-nci, n, l))
+    end
+    return xar, car, orbs
+end
+
+function make_array(x::Array{nGumqe}, op::Array{nGumqe_opt})
+    xar = Array{Float64}(undef, 0)
+    car = Array{Float64}(undef, 0)
+    orbs = Array{Tuple}(undef, 0)
+    for i = 1:size(x,1)
+        nci = size(car,1)
+        n = x[i].n
+        l = x[i].l
+        for name = fieldnames(typeof(x[i]))
+            name in [:n, :l] ? continue :
+            prop = getproperty(x[i], name)
+            opprop = getproperty(op[i], name)
+            if opprop isa Bool
+                if opprop
+                    append!(xar, prop)
+                    append!(car, fill(NaN, size(prop,1)))
+                else
+                    append!(car, prop)
+                end
+            else
+                for j = eachindex(opprop)
+                    if opprop[j]
+                        append!(xar, prop[j])
+                        append!(car, NaN)
+                    else
+                        append!(car, prop[j])
+                    end
+                end
+            end
+        end
+        push!(orbs, (size(car,1)-nci, n, l))
+    end
+    return xar, car, orbs
+end
+
+function make_wfstr_nGum(xcar::Array{Float64, 1}, orbs::Array{Tuple, 1})
     xc = Array{nGum}(undef, 0)
     for orb = orbs
         n, l = orb[2:3]
@@ -69,6 +215,37 @@ function make_wfstructs(xcar::Array{Float64, 1}, orbs::Array{Tuple, 1})
         c = [popfirst!(xcar) for _ = 1:ncd/2]
         d = [popfirst!(xcar) for _ = 1:ncd/2]
         push!(xc, nGum(a, n, l, r, c, d))
+    end
+    return xc
+end
+
+function make_wfstr_nGuma(xcar::Array{Float64, 1}, orbs::Array{Tuple, 1})
+    xc = Array{nGuma}(undef, 0)
+    for orb = orbs
+        n, l = orb[2:3]
+        nr = n - l
+        r = [popfirst!(xcar) for _ = 1:nr]
+        ncd = orb[1] - nr
+        a = [popfirst!(xcar) for _ = 1:ncd/3]
+        c = [popfirst!(xcar) for _ = 1:ncd/3]
+        d = [popfirst!(xcar) for _ = 1:ncd/3]
+        push!(xc, nGuma(n, l, r, a, c, d))
+    end
+    return xc
+end
+
+function make_wfstr_nGumqe(xcar::Array{Float64, 1}, orbs::Array{Tuple, 1})
+    xc = Array{nGumqe}(undef, 0)
+    for orb = orbs
+        n, l = orb[2:3]
+        a = popfirst!(xcar)
+        nr = n - l
+        r = [popfirst!(xcar) for _ = 1:nr]
+        ncd = orb[1] - (nr + 1)
+        c = [popfirst!(xcar) for _ = 1:ncd/3]
+        q = [popfirst!(xcar) for _ = 1:ncd/3]
+        d = [popfirst!(xcar) for _ = 1:ncd/3]
+        push!(xc, nGumqe(a, n, l, r, c, q, d))
     end
     return xc
 end
